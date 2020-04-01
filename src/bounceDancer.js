5485c93fd081bc7f8cc1bf930cf7c1aea06ef1f1
Charlie Thao <cthao309@gmail.com>
2020-03-31 18:10:40 -0700

    save before pull

--- src/bounceDancer.js
+++ src/bounceDancer.js
@@ -1,9 +1,8 @@
 var MakeBounceDancer = function(top, left, timeBetweenSteps) {
   this.oldStep = MakeDancer.prototype.step;
   MakeDancer.call(this, top, left, timeBetweenSteps);
+
   this.$node = $('<span class="dancer"><img class="animated bounceDancer" src="images/girl_dance.gif" title="" /></span>');
-  // $(this.$node).toggleClass('dancer ');
-  // $(this.$node).addClass('animated');
 }
 
 MakeBounceDancer.prototype = Object.create(MakeDancer.prototype);
